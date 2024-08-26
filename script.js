18414d8866182fa4fa9bd27e8b456b6db644299e
shabbir-ali123 <shabbiralykhan123@gmail.com>
2024-08-26 05:08:58 -1200

    changes

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
+document.querySelector(".hamburger").addEventListener("click", function () {
+  document.querySelector("nav ul").classList.toggle("show");
+});
 
-    document.querySelector(".hamburger").addEventListener("click", function () {
-      document.querySelector("nav ul").classList.toggle("show");
-    });
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav ul li a");
 
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("nav ul li a");
+window.addEventListener("scroll", () => {
+  let current = "";
 
-    window.addEventListener("scroll", () => {
-      let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
 
-      sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if (window.pageYOffset >= sectionTop - sectionHeight / 3) {
-          current = section.getAttribute("id");
-        }
-      });
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
 
-      navLinks.forEach((link) => {
-        link.classList.remove("active");
-        if (link.getAttribute("href").includes(current)) {
-          link.classList.add("active");
-        }
-      });
-    });
-
-    navLinks.forEach((link) => {
-      link.addEventListener("click", () => {
-        navLinks.forEach((link) => link.classList.remove("active"));
-        link.classList.add("active");
-      });
-    });
-  <
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((link) => link.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
